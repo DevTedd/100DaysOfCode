69220d483c2b9dfc8d11b3ad8c145c1768293452
Ted <kimanited@ymail.com>
2024-06-03 10:32:11 +0300

    Updates

---
+++ Day_23/File/main.py
@@ -0,0 +1,12 @@
+#This is an inbuilt method
+# with open("C:/Users/tkmwangi/Documents/GitHub/100DaysOfCode/Day_23/File I/text.txt") as file:
+# #file = open("C:/Users/tkmwangi/Documents/GitHub/100DaysOfCode/Day_23/File I/text.txt")
+#     contents = file.read()
+#     print(contents)
+#     file.close()
+    
+#with open("C:/Users/tkmwangi/Documents/GitHub/100DaysOfCode/Day_23/File I/text.txt", mode= 'a') as file:
+#    file.write("\nNew text, this is where ill add my hobbies")
+    
+with open('C:/Users/tkmwangi/Documents/GitHub/100DaysOfCode/Day_23/File/new_file.txt', mode = 'w') as file:
+    file.write("This is a new file") 
